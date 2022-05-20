f73e4309c6e74bb65428285f05ee6c24a2ef1cfd
Carlos Sánchez <oceanrdn@gmail.com>
2022-05-20 11:04:01 +0200

    Challenge 2 code

---
+++ challenges/challenge2.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge 2.
+ *
+ * Return an array with all the keys of the Object «person».
+ *
+ * @param {object} person
+ *
+ * Try to use a more functional approach using map/reduce/forEach, instead
+ * of a procedural loop (while, for).
+ */
+const getTheObjectKeys = person => {
+  console.log("Returning the keys of", person);
+
+  let objectKeys;
+
+  // YOUR CODE
+
+  return objectKeys;
+}
+
+module.exports = {
+  getTheObjectKeys,
+}
